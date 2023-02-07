87c25898d150e63f10ca25f7f162b94fba73430f
Leo Volwahsen <volwahsenl@gmail.com>
2023-02-07 21:20:34 +0100

    status nummer abfage beigefügt

--- controllers/plattenladens.js
+++ controllers/plattenladens.js
@@ -2,5 +2,5 @@ import * as Plattenladen from "../models/Plattenladen.js";
 
 export const getAll = async (req, res) => {
     const plattenladens = await Plattenladen.getAll();
-    res.json(plattenladens);
+    res.status(200).json(plattenladens);
 };
