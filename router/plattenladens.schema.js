27a1d3e9b4b1c93a88febee7d550f537759299d8
Leo Volwahsen <volwahsenl@gmail.com>
2023-02-14 01:22:24 +0100

    validate still not working jet

--- router/plattenladens.schema.js
+++ router/plattenladens.schema.js
@@ -1,22 +1,22 @@
-// export const postSchema = {
-//     type: "object",
-//     properties: {
-//         title: { type: "string" },
-//         artist: { type: "string" },
-//         year: { type: "number" },
-//         picture: { type: "string" },
-//         price: { type: "number" },
-//     },
-//     required: ["title", "artist", "year", "picture", "price"],
-//     additionalProperties: false,
-// };
+export const postSchema = {
+    type: "object",
+    properties: {
+        title: { type: "string" },
+        artist: { type: "string" },
+        year: { type: "number" },
+        picture: { type: "string" },
+        price: { type: "number" },
+    },
+    required: ["title", "artist", "year", "picture", "price"],
+    additionalProperties: false,
+};
 
 export const getSchema = {
     type: "object",
     additionalProperties: false,
 };
 
-// export const deleteSchema = {
-//     type: "object",
-//     additionalProperties: false,
-// };
+export const deleteSchema = {
+    type: "object",
+    additionalProperties: false,
+};
